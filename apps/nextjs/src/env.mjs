16cf750cdea705dfa7af768611e01dd2d688c5c7
Julius Marminge <julius0216@outlook.com>
2023-05-07 22:17:16 +0200

    feat: t3-env separation (#323)
    * add t3-env separation
    
    * fix issue with any env 0.2.2
    
    * manypkg
    
    * rm nodeenv
    
    * fix
    
    * cp example env in ci
    
    * ?
    
    * do it right
    
    * always ignore linting during build

--- apps/nextjs/src/env.mjs
+++ apps/nextjs/src/env.mjs
@@ -8,7 +8,6 @@ export const env = createEnv({
    */
   server: {
     DATABASE_URL: z.string().url(),
-    NODE_ENV: z.enum(["development", "test", "production"]),
   },
   /**
    * Specify your client-side environment variables schema here.
@@ -24,9 +23,9 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
+  skipValidation: !!process.env.CI,
 });
